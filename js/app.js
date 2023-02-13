2bbafac0208be77114169dc9e384d4a27246abc7
Giovannirighini2000 <righinigiovanni0@gmail.com>
2023-02-13 17:28:18 +0100

    feat added edit code

--- js/app.js
+++ js/app.js
@@ -13,20 +13,31 @@ alert(numeri.join(","));
 
 // timer inzio 30 secondi
 // IMPOSTA TIMER FUNZIONE CHE CHIEDE E CONFRONTA I NUMERI 
+const playbtn = document.getElementById('play')
+
 setTimeout(() => {
   let userNumeri = [];
-  for (let i = 0; i < 5; i++) {
-    userNumeri.push(parseInt(prompt(`Inserisci in ordine il primo numero fino all'ultimo appariranno altre richieste fino all'eseurimento dei numeri visti in precedenza  ${i + 1}:`)));
-  }
+  // funzione chiedoNumeriPersonavisti in precedenza
+  chiedoNumeriPersona();
 
-  // confronto dei numeri 
+  // confronto dei numeri funzione
   let numeriCorretti = [];
+  confrontoNumeri(userNumeri, numeriCorretti);
+//   stampa in console del risultato e dei numeri che sono Corretti  
+ 
+console.log(`Bravo hai individuato ${ numeriCorretti.length} numeri: ${ numeriCorretti.join(", ")}`);
+  // funzione chiedoNumeriPersona
+  function chiedoNumeriPersona() {
+    for (let i = 0; i < 5; i++) {
+      userNumeri.push(parseInt(prompt(`Inserisci in ordine il primo numero fino all'ultimo appariranno altre richieste fino all'eseurimento dei numeri visti in precedenza  ${i + 1}:`)));
+    }
+  }
+}, 30000);
+// funzione confrontoNumeri
+function confrontoNumeri(userNumeri, numeriCorretti) {
   for (let i = 0; i < 5; i++) {
     if (numeri.includes(userNumeri[i])) {
       numeriCorretti.push(userNumeri[i]);
     }
   }
-//   stampa in console del risultato e dei numeri che sono Corretti  
-
-  console.log(`Bravo hai individuato ${ numeriCorretti.length} numeri: ${ numeriCorretti.join(", ")}`);
-}, 30000);
+}
